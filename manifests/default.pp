9674b5c8a00878508aa330ae94cbf04005a4cae7
Gabriel Filion <gabster@lelutin.ca>
2015-02-10 07:32:58 -0500

    install pre-requisites
    this is normally handled by the debian package's dependencies.
    
    also: we have an issue with the install process: only the debian package
    tends to generate a local.sh file in /etc/alternc/

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,15 +1,74 @@
 Exec { path => '/bin:/sbin:/usr/bin:/usr/sbin' }
 
+# build depends
 package { 'make':
   ensure => installed,
+  before => Exec['build'],
+}
+
+# pre-requisites
+# see: apt-cache show alternc | grep "^Depends:\|^Pre-Depends:\|^Recommends:"
+package { [
+    # Depends
+    'apache2-mpm-itk',
+    'libapache2-mod-php5',
+    'php5-mysql',
+    'phpmyadmin',
+    'postfix',
+    'proftpd-mod-mysql',
+    'proftpd-basic',
+    'bind9',
+    'wget',
+    'rsync',
+    'ca-certificates',
+    'locales',
+    'perl',
+    'postfix-mysql',
+    'wwwconfig-common',
+    'sasl2-bin',
+    'libsasl2-modules',
+    'php5-cli',
+    'lockfile-progs',
+    'gettext',
+    'sudo',
+    'adduser',
+    'mysql-client',
+    'dnsutils',
+    'dovecot-common',
+    'dovecot-imapd',
+    'dovecot-pop3d',
+    'dovecot-mysql',
+    'vlogger',
+    'bsd-mailx',
+    'incron',
+    'cron',
+    'opendkim',
+    'opendkim-tools',
+    'dovecot-sieve',
+    'dovecot-managesieved',
+    'php5-curl',
+    # Pre-Depends
+    'debconf',
+    'bash',
+    'acl',
+    # Recommends
+    'mysql-server',
+    'ntp',
+    'quota',
+    'unzip',
+    'bzip2',
+  ]:
+  ensure => installed,
+  before => Exec['install'],
 }
 
 exec { 'build':
   command => 'sh -c "cd /vagrant/alternc; make build install-alternc"',
   creates => '/etc/alternc',
-  require => Package['make'],
 }
 
+# FIXME: alternc.install needs /etc/alternc/local.sh but this is only generated
+# by the debian package's postinstall script.
 exec { 'install':
   command => '/usr/share/alternc/install/alternc.install',
   creates => '/etc/alternc/bureau.conf',
