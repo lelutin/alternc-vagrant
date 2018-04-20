d0d54d4552a5d9272243a5d73697cd87f231aec4
Gabriel Filion <gabster@lelutin.ca>
2018-04-20 03:10:52 -0400

    finish automating alternc installation
    by installing the alternc package manually, I got the missing
    "selections".
    
    by running alternc.install manually I also saw that it would crash
    without an error message if the hostname configured by the package was
    not a hostname that the script liked (as I've written in comments)
    
    With this, we now get an alternc control panel on the VM's IP and we can
    login with admin/admin !

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,8 +1,25 @@
 Exec { path => '/bin:/sbin:/usr/bin:/usr/sbin' }
 
-# pre-requisites. dpkg method doesn't automatically bring in dependencies
-# Those were taken from ./alternc/debian/control in sections "pre-depends" and
-# "depends", minus packages that are usually installed in debian netinst.
+# This manifest installs AlternC in an unattended way.
+# It will install a package that's in /vagrant, so this means you need to have
+# the alternc source code checked out under here and to have compiled packages.
+# See the README for command examples
+#
+# In order to test changes, you need to compile packages after each code change.
+#
+# For the install procedure to run successfully, you need to have a
+# fully-qualified domain name that's "valid" enough for alternc. so that means
+# you need something that has a dot in it. Make sure your vagrant box sets this
+# up correctly.
+#
+# Once vagrant is done provisioning, check out "vagrant ssh-config" to find
+# your VM's IP. You can use that IP to access AlternC's control panel in HTTP.
+
+# pre-requisites. dpkg method used for alternc package doesn't automatically
+# bring in dependencies Those were taken from ./alternc/debian/control in
+# sections "pre-depends" and "depends", minus packages that are usually
+# installed in debian netinst, plus mariadb which is only suggested (you could
+# tell the package to use a remote database).
 package { [
   'acl',
   'debianutils',
@@ -46,6 +63,7 @@ package { [
   'php7.0-curl',
   'quota',
   'pwgen',
+  'mariadb-server',
 ]:
   ensure => installed,
   before => Exec['preseeding'],
@@ -58,32 +76,45 @@ package { [
 #
 # Of course, you can also vagrant destroy and bring up again.
 $preseed_items = @("END")
-  alternc alternc/hostingname string debian9
-  alternc alternc/desktopname string bureau.example.com
-  alternc alternc/internal_ip string 127.0.0.1
+  # alternc general settings
+  alternc alternc/hostingname string AlternC
+  alternc alternc/desktopname string ${::fqdn}
+  alternc alternc/internal_ip string ${::ipaddress}
   alternc alternc/public_ip string ${::ipaddress}
   alternc alternc/use_private_ip boolean true
   alternc alternc/alternc_html string /var/www/alternc
   alternc alternc/alternc_mail string /var/mail/alternc
   alternc alternc/alternc_logs string /var/log/alternc/sites
-  alternc alternc/ns1 string ns1.example.com
-  alternc alternc/ns2 string ns2.example.com
-  alternc alternc/mysql/host string localhost
+  alternc alternc/monitor_ip string 
+  alternc alternc/alternc_location string 
+  alternc alternc/postrm_remove_datafiles boolean 
+  alternc alternc/slaves string 
+  # dns settings
+  alternc alternc/ns1 string ${::hostname}
+  alternc alternc/ns2 string ${::hostname}
+  alternc alternc/postrm_remove_bind boolean 
+  # mysql settings
+  alternc alternc/use_local_mysql boolean true
+  alternc alternc/mysql/host string 127.0.0.1
   alternc alternc/mysql/db string alternc
+  alternc alternc/mysql/client string localhost
   alternc alternc/mysql/user string sysusr
-  alternc alternc/mysql/password password blahblah1
+  alternc alternc/mysql/password password blablah2
   alternc alternc/mysql/alternc_mail_user string alternc_user
   alternc alternc/mysql/alternc_mail_password password blablah2
-  alternc alternc/mysql/remote_user string sysusr
-  alternc alternc/mysql/remote_password password blahblah3
-  alternc alternc/mysql/client string %
+  alternc alternc/use_remote_mysql boolean 
+  alternc alternc/mysql/remote_user string 
+  alternc alternc/mysql/remote_password password 
+  alternc alternc/retry_remote_mysql boolean 
   alternc alternc/sql/backuptype string rotate
   alternc alternc/sql/backupoverwrite string no
-  alternc alternc/use_local_mysql boolean true
-  alternc alternc/use_remote_mysql boolean false
-  alternc alternc/retry_remote_mysql boolean false
-  alternc alternc/alternc_mail string mail.example.com
-  alternc alternc/default_mx string mx.example.com
+  alternc alternc/postrm_remove_databases boolean 
+  # email settings
+  alternc alternc/default_mx string ${::hostname}
+  alternc alternc/default_mx2 string 
+  alternc alternc/postrm_remove_mailboxes boolean 
+  # quota settings
+  alternc alternc/quotauninstalled note false
   | END
 exec { 'preseeding':
   command => "echo -e \"${preseed_items}\" | debconf-set-selections",
@@ -95,5 +126,12 @@ package { 'alternc':
   provider => 'dpkg',
   source   => '/vagrant/alternc_3.3.10_all.deb',
   require  => Exec['preseeding'],
+  notify   => Exec['alternc.install'],
+}
+
+# Finish installation: the package doesn't finish its job
+exec { 'alternc.install':
+  command     => '/usr/sbin/alternc.install',
+  refreshonly => true,
 }
 
